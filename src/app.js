2e036b6c13b8deb8e602f7ac57afc6a977b7c577
GJoao24 <122391147+GJoao24@users.noreply.github.com>
2025-05-21 15:28:14 +0000

    generadordeDominio

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let dominio = ['.com', '.es']
+  
+  const dominioFinal = []
+  for (let pronouns  = 0; pronouns < pronoun.length; pronouns++) {
+    for (let adjs = 0; adjs < adj.length; adjs++) {
+      for (let nouns = 0; nouns < noun.length; nouns++) {
+        for (let dominios = 0; dominios < dominio.length; dominios++) {
+           dominioFinal.push (pronoun[pronouns] + adj[adjs] + noun[nouns] + dominio[dominios])
+          
+        }
+        
+      }
+      
+    }
+    
+  }
+  
+  console.log(dominioFinal)
   console.log("Hello Rigo from the console!");
+  document.getElementById ("dominioFinal").textContent = dominioFinal;
 };
